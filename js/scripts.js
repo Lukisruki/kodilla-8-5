59a6797a0783433c1960763dfdb8e39ae6827882
lukasz kryst <lukaszkryst@gmail.com>
2018-02-26 12:21:39 +0100

    Add alert

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,7 @@
-var a = 1,
-    h = 2,
+var a = 2,
+    h = 3,
     triangleArea = a * h / 2;
     
-console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
+alert(triangleArea);
+
+/* console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea); */
